86e6999570ea95a4bf34e6d975406e83970b4b84
Gustavo <gumastro@usp.br>
2019-06-21 19:13:59 -0300

    Changes in font-size

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,6 +2,7 @@ class Pet < ApplicationRecord
   has_one_attached :image
   belongs_to :owner
   has_one :volunteer
+  validate :correct_image_type
 
     def idade
 
@@ -14,7 +15,16 @@ class Pet < ApplicationRecord
     end
 
     def idade_mes
-        
+
         return (Date.today.month - self.birthdate.month)%12
+    end
+
+
+    def correct_image_type
+        if image.attached? && !image.content_type.in?(%w(image/jpeg image/png image/jpg))
+            errors.add(:image, 'must be a JPEG or PNG or JPG!')
+        elsif image.attached? == false
+            errors.add(:image, 'required!')
+        end
     end
 end
